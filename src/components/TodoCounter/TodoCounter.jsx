6630a534df75367ea4118969d5c430c845b21514
Juan Obando <juanobando.developer@gmail.com>
2023-05-03 15:00:01 -0500

    feat: add todoCounter component with styles

---
+++ src/components/TodoCounter/TodoCounter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "../../styles/TodoCounter.module.css";
+
+export function TodoCounter() {
+  return <h2 className={styles.title}>Has completado 2 de 3 TODOs</h2>;
+}
