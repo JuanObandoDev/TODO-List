ed22e709127eb416cecc32ac72d00e297c9d99e8
Juan Obando <juanobando.developer@gmail.com>
2023-05-03 15:02:25 -0500

    feat: add todoItem component with styles

---
+++ src/components/TodoItem/TodoItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "../../styles/TodoItem.module.css";
+
+export function TodoItem(props) {
+    return (
+        <li className={styles.item}>
+            <span className={`${styles.checkIcon} ${styles.icon} ${props.completed && styles.checkIconActive}`}>√</span>
+            <p className={`${styles.description} ${props.completed && styles.descriptionCrossOut}`}>{props.text}</p>
+            <span className={`${styles.icon} ${styles.deleteIcon}`}>X</span>
+        </li>
+    );
+}
