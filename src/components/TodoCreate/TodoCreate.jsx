1f83fad717dd536ebb3ff914280006bd63ea4539
Juan Obando <juanobando.developer@gmail.com>
2023-05-03 15:00:56 -0500

    feat: add todoCreate component with styles

---
+++ src/components/TodoCreate/TodoCreate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "../../styles/TodoCreate.module.css";
+
+export function TodoCreate() {
+    return <button className={styles.createTodoBtn}>+</button>;
+}
