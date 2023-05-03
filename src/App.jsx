eb0d145f165995fd40939460bdc830fbd0dc3dc7
Juan Obando <juanobando.developer@gmail.com>
2023-05-03 16:11:05 -0500

    feat: import searchContextProvider, render searchContextProvider component and make todoList map readable

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import { TodoCounter } from './components/TodoCounter/TodoCounter'
-import { TodoSearch } from './components/TodoSearch/TodoSearch'
-import { TodoList } from './components/TodoList/TodoList'
-import { TodoItem } from './components/TodoItem/TodoItem'
-import { TodoCreate } from './components/TodoCreate/TodoCreate'
+import { TodoCounter } from "./components/TodoCounter/TodoCounter";
+import { TodoSearch } from "./components/TodoSearch/TodoSearch";
+import { TodoList } from "./components/TodoList/TodoList";
+import { TodoItem } from "./components/TodoItem/TodoItem";
+import { TodoCreate } from "./components/TodoCreate/TodoCreate";
+import { SearchContextProvider } from "./contexts/searchContext/searchContext";
 
-import './styles/App.css'
+import "./styles/App.css";
 
 const todos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el curso de intro a React", completed: false },
+  { text: "Llorar con la llorona", completed: false },
 ];
 
 function App() {
   return (
     <>
       <TodoCounter />
-      <TodoSearch />
+      <SearchContextProvider>
+        <TodoSearch />
+      </SearchContextProvider>
       <TodoList>
-        {todos.map(todo => (
-          <TodoItem text={todo.text} key={todo.text} completed={todo.completed} />
+        {todos.map((todo) => (
+          <TodoItem
+            text={todo.text}
+            key={todo.text}
+            completed={todo.completed}
+          />
         ))}
       </TodoList>
       <TodoCreate />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
