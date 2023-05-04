8f5a0420c48c0f63362e44d7e43ac113019f6dbe
Juan Obando <juanobando.developer@gmail.com>
2023-05-04 13:19:14 -0500

    feat: add searchedTodos state

--- src/contexts/searchContext/searchContext.jsx
+++ src/contexts/searchContext/searchContext.jsx
@@ -4,9 +4,12 @@ export const SearchContext = createContext();
 
 export const SearchContextProvider = ({ children }) => {
   const [search, setSearch] = useState("");
+  const [searchedTodos, setSearchedTodos] = useState([]);
 
   return (
-    <SearchContext.Provider value={{ search, setSearch }}>
+    <SearchContext.Provider
+      value={{ search, setSearch, searchedTodos, setSearchedTodos }}
+    >
       {children}
     </SearchContext.Provider>
   );
