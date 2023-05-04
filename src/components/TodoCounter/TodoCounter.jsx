7cbcb1f9deffa06be35d82f397ec43301cd44dcf
Juan Obando <juanobando.developer@gmail.com>
2023-05-04 13:15:03 -0500

    feat: import useCounterTodos custom hook and use in the title

--- src/components/TodoCounter/TodoCounter.jsx
+++ src/components/TodoCounter/TodoCounter.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { useCounterTodos } from "../../hooks/useCounterTodos/useCounterTodos";
+
 import styles from "../../styles/TodoCounter.module.css";
 
 export function TodoCounter() {
-  return <h2 className={styles.title}>Has completado 2 de 3 TODOs</h2>;
+  return (
+    <h2 className={styles.title}>
+      Has completado {useCounterTodos().totalCompletedTodos} de{" "}
+      {useCounterTodos().totalTodos} TODOs
+    </h2>
+  );
 }
