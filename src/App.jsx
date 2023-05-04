08f8c01448f4a511f93cfd792e3d0aa65649fe77
Juan Obando <juanobando.developer@gmail.com>
2023-05-04 13:24:40 -0500

    feat: import todosContextProvider
    refactor: move todo.map to todoList component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
 import { TodoCounter } from "./components/TodoCounter/TodoCounter";
 import { TodoSearch } from "./components/TodoSearch/TodoSearch";
 import { TodoList } from "./components/TodoList/TodoList";
-import { TodoItem } from "./components/TodoItem/TodoItem";
 import { TodoCreate } from "./components/TodoCreate/TodoCreate";
+
 import { SearchContextProvider } from "./contexts/searchContext/searchContext";
+import { TodosContextProvider } from "./contexts/todosContext/todosContext";
 
 import "./styles/App.css";
 
-const todos = [
-  { text: "Cortar cebolla", completed: true },
-  { text: "Tomar el curso de intro a React", completed: false },
-  { text: "Llorar con la llorona", completed: false },
-];
-
 function App() {
   return (
-    <>
+    <TodosContextProvider>
       <TodoCounter />
       <SearchContextProvider>
         <TodoSearch />
+        <TodoList />
       </SearchContextProvider>
-      <TodoList>
-        {todos.map((todo) => (
-          <TodoItem
-            text={todo.text}
-            key={todo.text}
-            completed={todo.completed}
-          />
-        ))}
-      </TodoList>
       <TodoCreate />
-    </>
+    </TodosContextProvider>
   );
 }
 
