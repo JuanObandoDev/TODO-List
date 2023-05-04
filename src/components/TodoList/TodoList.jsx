5229aa10d70e0fdfc8af0ff2b12c5cd37964e51d
Juan Obando <juanobando.developer@gmail.com>
2023-05-04 13:17:37 -0500

    feat: import todoItem component, useSearchedTodos custom hook and render todoItems

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
+import { TodoItem } from "../TodoItem/TodoItem";
+
+import { useSearchedTodos } from "../../hooks/useSearchedTodos/useSearchedTodos";
+
 import styles from "../../styles/TodoList.module.css";
 
-export function TodoList(props) {
+export function TodoList() {
   return (
     <section>
-      <ul className={styles.cards}>{props.children}</ul>
+      <ul className={styles.cards}>
+        {useSearchedTodos().searchedTodos.map((todo) => (
+          <TodoItem
+            text={todo.text}
+            key={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
