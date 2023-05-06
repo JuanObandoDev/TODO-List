130786f1d4b6fbca6f5539ea3a940f1e07d1ca58
Juan Obando <juanobando.developer@gmail.com>
2023-05-06 17:52:06 -0500

    refactor: delete useSearchedTodos custom hook and move his function into searchContextProvider

--- src/contexts/searchContext/searchContext.jsx
+++ src/contexts/searchContext/searchContext.jsx
@@ -1,10 +1,24 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
+
+import { TodosContext } from "../todosContext/todosContext";
 
 export const SearchContext = createContext();
 
 export const SearchContextProvider = ({ children }) => {
+  const { todos } = useContext(TodosContext);
   const [search, setSearch] = useState("");
   const [searchedTodos, setSearchedTodos] = useState([]);
+
+  useEffect(() => {
+    if (search.length >= 1) {
+      const filteredTodos = todos.filter((todo) => {
+        return todo.text.toLowerCase().includes(search.toLowerCase());
+      });
+      setSearchedTodos(filteredTodos);
+    } else {
+      setSearchedTodos(todos);
+    }
+  }, [search, todos, setSearchedTodos]);
 
   return (
     <SearchContext.Provider
