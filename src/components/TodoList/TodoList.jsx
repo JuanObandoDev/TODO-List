58bb1784a747761bdde3411cdb99d5dc50d76be8
Juan Obando <juanobando.developer@gmail.com>
2023-05-06 17:51:01 -0500

    refactor: remove useSearchedTodos custom hook and just import searchedTodos context

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { TodoItem } from "../TodoItem/TodoItem";
 
-import { useSearchedTodos } from "../../hooks/useSearchedTodos/useSearchedTodos";
+import { SearchContext } from "../../contexts/searchContext/searchContext";
 
 import styles from "../../styles/TodoList.module.css";
 
 export function TodoList() {
+  const { searchedTodos } = useContext(SearchContext);
+
   return (
     <section>
       <ul className={styles.cards}>
-        {useSearchedTodos().searchedTodos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             text={todo.text}
             key={todo.text}
