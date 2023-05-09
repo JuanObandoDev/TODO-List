d1bc0902876f7ab111fd753c90f2d1b3227cd012
Juan Obando <juanobando.developer@gmail.com>
2023-05-09 15:11:35 -0500

    feat: import TodosContext and toggle open and close modal

--- src/components/TodoCreate/TodoCreate.jsx
+++ src/components/TodoCreate/TodoCreate.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { TodosContext } from "../../contexts/todosContext/todosContext";
 
 import styles from "../../styles/TodoCreate.module.css";
 
 export function TodoCreate() {
-  return <button className={styles.createTodoBtn}>+</button>;
+  const { openModal, setOpenModal } = useContext(TodosContext);
+  return (
+    <button
+      className={styles.createTodoBtn}
+      onClick={() => (openModal ? setOpenModal(false) : setOpenModal(true))}
+    >
+      +
+    </button>
+  );
 }
