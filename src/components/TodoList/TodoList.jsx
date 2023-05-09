935eccc5367f4842ba5a68339b37fbae0bd1ea90
Juan Obando <juanobando.developer@gmail.com>
2023-05-09 18:17:11 -0500

    feat: import todosLoading and emptyTodos component to render it and todosContext

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,17 +1,24 @@
 import React, { useContext } from "react";
 
 import { TodoItem } from "../TodoItem/TodoItem";
+import { TodosLoading } from "../TodosLoading/TodosLoading";
+import { EmptyTodos } from "../EmptyTodos/EmptyTodos";
 
 import { SearchContext } from "../../contexts/searchContext/searchContext";
+import { TodosContext } from "../../contexts/todosContext/todosContext";
 
 import styles from "../../styles/TodoList.module.css";
 
 export function TodoList() {
+  const { loading } = useContext(TodosContext);
   const { searchedTodos } = useContext(SearchContext);
 
   return (
     <section>
       <ul className={styles.cards}>
+        {loading && <TodosLoading />}
+        {!loading && !searchedTodos.length && <EmptyTodos />}
+
         {searchedTodos.map((todo) => (
           <TodoItem
             text={todo.text}
