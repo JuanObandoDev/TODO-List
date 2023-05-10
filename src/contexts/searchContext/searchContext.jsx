8e6bf5ff860adca08ea6ec770d8d5a581c56433c
Juan Obando <juanobando.developer@gmail.com>
2023-05-10 12:59:20 -0500

    refactor: remove unnecesary Effect

--- src/contexts/searchContext/searchContext.jsx
+++ src/contexts/searchContext/searchContext.jsx
@@ -1,29 +1,30 @@
-import React, { createContext, useState, useContext, useEffect } from "react";
+import React, { createContext, useState, useContext, useMemo } from "react";
 
 import { TodosContext } from "../todosContext/todosContext";
+
+const getFilteredTodos = (search, todos) => {
+  if (search.length >= 1) {
+    const filteredTodos = todos.filter((todo) => {
+      return todo.text.toLowerCase().includes(search.toLowerCase());
+    });
+    return filteredTodos;
+  } else {
+    return todos;
+  }
+};
 
 export const SearchContext = createContext();
 
 export const SearchContextProvider = ({ children }) => {
   const { todos } = useContext(TodosContext);
   const [search, setSearch] = useState("");
-  const [searchedTodos, setSearchedTodos] = useState([]);
-
-  useEffect(() => {
-    if (search.length >= 1) {
-      const filteredTodos = todos.filter((todo) => {
-        return todo.text.toLowerCase().includes(search.toLowerCase());
-      });
-      setSearchedTodos(filteredTodos);
-    } else {
-      setSearchedTodos(todos);
-    }
-  }, [search, todos, setSearchedTodos]);
+  const searchedTodos = useMemo(
+    () => getFilteredTodos(search, todos),
+    [search, todos]
+  );
 
   return (
-    <SearchContext.Provider
-      value={{ search, setSearch, searchedTodos, setSearchedTodos }}
-    >
+    <SearchContext.Provider value={{ search, setSearch, searchedTodos }}>
       {children}
     </SearchContext.Provider>
   );
